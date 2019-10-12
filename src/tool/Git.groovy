101ccaea65bd2777e1067e7a3348c4b53bc633c2
Victor Amorim <victor.amsantos@gmail.com>
2019-10-12 14:48:34 -0300

    Improvments

--- src/tool/Git.groovy
+++ src/tool/Git.groovy
@@ -4,10 +4,10 @@ import utils.Sh
 
 class Git extends Sh{
     String cloneAndCheckout(String REPO, String REPO_NAME, String BRANCH) {
-        script{
+
             println("cloneAndChekcout aqui")
             exec("git clone -b ${BRANCH} ${REPO} && cd ${REPO_NAME} && git checkout ${BRANCH}")
             return super.execWithReturn("cd ${REPO_NAME} && pwd")
-        }
+
     }
 }
