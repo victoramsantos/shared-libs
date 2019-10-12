67aa8f4a8fd55ca509e54c19c24d63f3445faf4d
Victor Amorim <victor.amsantos@gmail.com>
2019-10-12 14:30:17 -0300

    Improvments

--- src/build/Pip.groovy
+++ src/build/Pip.groovy
@@ -1,7 +1,7 @@
 package build
 
 class Pip {
-    void install(String pythonVersion, String requirements){
+    void install(String pythonVersion, String requirements="requirements"){
         sh "pip${pythonVersion} install -r ${requirements} -t ."
     }
 }
