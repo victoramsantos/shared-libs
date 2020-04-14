d32ce1d29339a60b30e27be36d9c58372b7b6385
Victor Amorim Dos Santos <victor.amsantos@gmail.com>
2020-04-14 15:20:17 -0300

    testing

--- src/pipeline/handler/ci/PipelineHandlerCiFactory.groovy
+++ src/pipeline/handler/ci/PipelineHandlerCiFactory.groovy
@@ -1,12 +1,14 @@
 package pipeline.handler.ci
 
 import libs.utils.ApplicationProperties
+import libs.utils.Log
 import pipeline.handler.ci.language.PythonCi
 
 class PipelineHandlerCiFactory {
 
     static PipelineHandlerCi build(ApplicationProperties properties){
         PipelineHandlerCi pipelineHandlerCi
+        Log.info(properties.getString("PIPELINE_CI"))
         CiTypes ciTypes = CiTypes.valueOf(properties.getString("PIPELINE_CI"))
 
         switch (ciTypes){
