b2ebf050ef166346ac85a59f9f6511a3f01b7db2
Victor Amorim Dos Santos <victor.amsantos@gmail.com>
2020-04-14 15:27:17 -0300

    testing

--- src/libs/utils/ApplicationProperties.groovy
+++ src/libs/utils/ApplicationProperties.groovy
@@ -3,11 +3,18 @@ package libs.utils
 class ApplicationProperties {
     private Map properties
     ApplicationProperties(Map params){
+        printMap(params)
         this.properties = new HashMap(params)
+        printMap(this.properties)
     }
 
     String getString(String key)  {
         return this.properties.get(key) as String
     }
 
+    static void printMap(Map map){
+        for(Map.Entry entry: map.entrySet()){
+            print("${entry.key}--${entry.value}")
+        }
+    }
 }
