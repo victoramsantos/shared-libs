4a57eda3b0aca2e0b73a912c5d6aac15562b93f4
Victor Amorim Dos Santos <victor.amsantos@gmail.com>
2020-04-14 17:32:01 -0300

    testing

--- src/libs/docker/Docker.groovy
+++ src/libs/docker/Docker.groovy
@@ -1,7 +1,6 @@
 package libs.docker
 
 interface Docker {
-    void login(String region)
     void createRepository(String repository, String applicationName)
     void build(String repository, String applicationName, String tag)
     void push(String repository, String applicationName, String tag)
--- src/libs/docker/DockerEcr.groovy
+++ src/libs/docker/DockerEcr.groovy
@@ -3,8 +3,8 @@ package libs.docker
 import static pipeline.context.PipelineContext.shell
 
 class DockerEcr implements Docker {
-    @Override
-    void login(String region){
+
+    DockerEcr(String region){
         shell("\$(aws ecr get-login --region $region --no-include-email)")
     }
 
