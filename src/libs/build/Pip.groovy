62bad48eef89d31ac688071c612d034e131db51b
Victor Amorim Dos Santos <victor.amsantos@gmail.com>
2020-04-14 16:50:33 -0300

    testing

--- src/libs/build/Pip.groovy
+++ src/libs/build/Pip.groovy
@@ -5,10 +5,10 @@ import static pipeline.context.PipelineContext.shell
 
 class Pip {
     void install(String requirements){
-        shell("pip install -r $requirements")
+        shell("pip install -r $requirements -t . --no-cache-dir")
     }
 
     void installDependency(String dependency){
-        shell("pip install $dependency")
+        shell("pip install $dependency -t . --no-cache-dir")
     }
 }
