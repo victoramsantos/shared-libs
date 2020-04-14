82765346e47b6a030874fb5c33d69cd8fabb746a
Victor Amorim Dos Santos <victor.amsantos@gmail.com>
2020-04-14 15:36:50 -0300

    testing

--- src/libs/utils/ApplicationProperties.groovy
+++ src/libs/utils/ApplicationProperties.groovy
@@ -4,6 +4,8 @@ class ApplicationProperties {
     private Map properties
     ApplicationProperties(Map params){
         this.properties = params
+        print(this.properties["PIPELINE_CI"])
+        print(this.properties.get("PIPELINE_CI"))
     }
 
     String getString(String key)  {
