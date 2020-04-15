39143e41d3f37d47cdc5851c1e71e8563ee7f0f3
Victor Amorim Dos Santos <victor.amsantos@gmail.com>
2020-04-15 09:07:55 -0300

    testing

---
+++ vars/GenericCd.groovy
@@ -0,0 +1,57 @@
+import libs.utils.ApplicationProperties
+import libs.utils.Utils
+import pipeline.context.PipelineContext
+import pipeline.handler.ci.PipelineHandlerCi
+import pipeline.handler.ci.PipelineHandlerCiFactory
+
+def call(_) {
+    Map defaultProperties = Utils.parsePropertyFile(this,"default.properties")
+    ApplicationProperties applicationProperties = new ApplicationProperties(defaultProperties, jobParams)
+    PipelineHandlerCi pipelineHandler = PipelineHandlerCiFactory.build(applicationProperties)
+    PipelineContext.init(this)
+
+    pipeline {
+        agent any
+        stages {
+            stage("Cloning project") {
+                steps {
+                    script {
+                        pipelineHandler.scmClone()
+                    }
+                }
+            }
+            stage("Building and testing") {
+                agent {
+                    docker {
+                        image "${pipelineHandler.getBuildImage()}"
+                        args "-v ${WORKSPACE}:${WORKSPACE} -w ${WORKSPACE}"
+                    }
+                }
+                steps {
+                    dir("${applicationProperties.getString("APPLICATION_PATH")}") {
+                        script {
+                            pipelineHandler.buildAndTest()
+                        }
+                    }
+                }
+            }
+            stage("Building image") {
+                steps {
+                    dir("${applicationProperties.getString("APPLICATION_PATH")}") {
+                        script{
+                            pipelineHandler.buildDockerImage()
+                        }
+                    }
+                }
+            }
+        }
+        post {
+            always {
+                cleanWs()
+            }
+            success {
+
+            }
+        }
+    }
+}
---
+++ vars/test/testing2.groovy
@@ -0,0 +1,29 @@
+package test
+
+import libs.utils.ApplicationProperties
+import libs.utils.Utils
+
+def call(Map jobParams) {
+    Map defaultProperties = Utils.parsePropertyFile(this,"default.properties")
+    ApplicationProperties applicationProperties = new ApplicationProperties(defaultProperties, jobParams)
+
+    pipeline {
+        agent any
+        stages {
+            stage("works") {
+                steps {
+                    script {
+                        for (Map.Entry entry : applicationProperties.getApplicationProperties().entrySet()) {
+                            print("${entry.key} == ${entry.value}")
+                        }
+                    }
+                }
+            }
+        }
+        post {
+            always {
+                cleanWs()
+            }
+        }
+    }
+}
