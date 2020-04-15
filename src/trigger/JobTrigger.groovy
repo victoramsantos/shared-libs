4715124db68d9f62271b40bb3293a501f2562bab
Victor Amorim Dos Santos <victor.amsantos@gmail.com>
2020-04-15 09:44:45 -0300

    testing

--- src/trigger/JobTrigger.groovy
+++ src/trigger/JobTrigger.groovy
@@ -5,18 +5,18 @@ import libs.utils.Utils
 
 class JobTrigger {
     static void trigger(def context, ApplicationProperties applicationProperties) {
-        String job = ""
+        String job = selectJob(applicationProperties)
         String jsonMap = Utils.parseMapToJson(applicationProperties.properties)
 
         context.build(
-                job: "${}",
+                job: job,
                 parameters: [
                         [$class: 'StringParameterValue', name: 'jsonMap', value: jsonMap]
                 ]
         )
     }
 
-    private static String selectJob(){
-        return ""
+    private static String selectJob(ApplicationProperties applicationProperties){
+        return applicationProperties.getString("GENERIC_CD_JOB")
     }
 }
